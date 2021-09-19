ff004328f1a57eee38d6c6d0accfa1aa6d533ba8
philipKovachev99 <sdfsffsdf642@gmail.com>
2021-09-19 18:52:35 +0300

    created the basic grid and added paging + filtering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
-
+import DataGrid  from './components/DataGrid';
 function App() {
   return (
     <div className="App">
-      
+     <DataGrid/>
     </div>
   );
 }
---
+++ src/components/DataGrid.js
@@ -0,0 +1,45 @@
+
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import { process } from "@progress/kendo-data-query";
+import React from 'react';
+import flightData from '../data/flightData.json';
+
+ export default function DataGrid() {
+  const [dataState, setDataState] = React.useState({ skip: 0, take: 5 });
+  const [result, setResult] = React.useState(process(flightData, dataState));
+
+  const onDataStateChange = (event) => {
+    setDataState(event.dataState);
+    setResult(process(flightData, event.dataState));
+  }
+
+    return (
+      <div className="App">
+   <Grid
+      data={result}
+      filterable={true}
+      onDataStateChange={onDataStateChange}
+      pageable={true}
+      total={flightData.length}
+      {...dataState}
+    >
+     <GridColumn field='code' title='Code'/>
+     <GridColumn field='lat' title='Latitude'/>
+     <GridColumn field='lon' title='Longitude'/>
+     <GridColumn field='name' title='Name'/>
+     <GridColumn field='city' title='City'/>
+     <GridColumn field='state' title='State'/>
+     <GridColumn field='country' title='Country'/>
+     <GridColumn field='woeid' title='Woeid'/>
+     <GridColumn field='tz' title='TZ'/>
+     <GridColumn field='type' title='Type'/>
+     <GridColumn field='runway_length' title='Runaway Length'/>
+     <GridColumn field='elev' title='Elevation'/>
+     <GridColumn field='icao' title='Icao'/>
+     <GridColumn field='direct_flights' title='Direct Flights'/>
+     <GridColumn field='carriers' title='Carriers'/>
+    </Grid>
+      </div>
+    );
+  }
+
